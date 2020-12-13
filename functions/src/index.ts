d94c920d2efd7d81881f9c68d250a11ace6da857
Solant <runner62v6@gmail.com>
2020-12-13 22:08:06 +0300

    fix: handle google audit log

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,5 +5,10 @@ import createBot from './bot';
 const bot = createBot();
 
 export const handler = functions.https.onRequest(async (request, response) => {
-    await bot.handleUpdate(request.body, response);
+    const res = await bot.handleUpdate(request.body, response);
+
+    // handle google audit log
+    if (!res) {
+        response.sendStatus(200);
+    }
 });
